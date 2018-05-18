bf78654e1e911039a1d29a9d5f865f1d5cefe765
Rachel Welt <rwelt@amnh.org>
2018-05-18 14:16:29 -0400

    hi
    hi

---
+++ R/test.R
@@ -0,0 +1,21 @@
+#' @import raster
+#' @import parallel
+#' @import stats
+NULL
+
+
+#' Echo
+#' 
+#' This function echos whatever you give it.
+#' 
+#' @param echo A word or sentence to echo
+#'
+#' @export
+#' @examples
+#'
+#' echo('This is a test')
+
+
+echo = function(echo){
+  return(echo)
+}
